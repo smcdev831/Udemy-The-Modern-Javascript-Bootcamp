c8406b0499e8f2213b6f51986ed84899f53b263a
smcdev <smcdev@outlook.com>
2022-12-18 21:55:03 -0600

    Update Booleans and Comparison Operators.js

--- Basics/Booleans and Comparison Operators.js
+++ Basics/Booleans and Comparison Operators.js
@@ -1,1 +1,9 @@
-let isFreezing = true;
+let temp = 32;
+let isFreezing = temp <= 32;
+console.log(isFreezing);
+
+//Challenge
+let age = 40;
+let isChild = age <= 7;
+let isSenior = age >= 65;
+console.log(isChild, isSenior);
