02e1046e0c2131b6a8c73bc70c5defda7b09130d
smcdev <smcdev@outlook.com>
2022-12-18 21:04:47 -0600

    Update Numbers.js

--- Basics/Numbers.js
+++ Basics/Numbers.js
@@ -12,3 +12,11 @@ console.log(num);
 
 num = 5.5 / 2;
 console.log(num);
+
+let x = 11;
+num = (x + 1) * 2;
+console.log(num);
+
+let age = 40;
+let dogYears = (age + 1) / 7;
+console.log(dogYears);
