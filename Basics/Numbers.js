f80cf0a84f1f2ace0014be933b1918f3f82aa0b9
smcdev <smcdev@outlook.com>
2022-12-18 20:59:46 -0600

    Update Numbers.js

---
+++ Basics/Numbers.js
@@ -0,0 +1,14 @@
+let num = 5.5;
+console.log(num);
+
+num = 5.5 + 2;
+console.log(num);
+
+num = 5.5 - -1;
+console.log(num);
+
+num = 5.5 * 2;
+console.log(num);
+
+num = 5.5 / 2;
+console.log(num);
