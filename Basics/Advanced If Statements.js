d4c5dd75910fff8ba477f736919c8d50011033f5
smcdev <smcdev@outlook.com>
2022-12-18 22:20:09 -0600

    Update Advanced If Statements.js

--- Basics/Advanced If Statements.js
+++ Basics/Advanced If Statements.js
@@ -1,7 +1,12 @@
 let isAccountLocked = false;
+let userRole = "Admin";
 
 if (isAccountLocked === true) {
   console.log("This account is locked");
+} else if (userRole === "Admin") {
+  console.log("Welcome Admin!");
 } else {
-  console.log("This account is not locked");
+  console.log("Welcome!");
 }
+
+//Challenge
