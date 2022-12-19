c8a162f1714c41f9cdd5b4af6baa139c33ca5b49
smcdev <smcdev@outlook.com>
2022-12-18 20:53:20 -0600

    Update Strings and variables.js

--- Basics/Strings and variables.js
+++ Basics/Strings and variables.js
@@ -5,3 +5,9 @@ let firstName = "Sharon";
 let lastName = "M";
 let fullName = firstName + " " + lastName;
 console.log(fullName);
+
+//Challenge
+let city = "Austin";
+let country = "United States of America";
+let location = city + ", " + country;
+console.log(location);
