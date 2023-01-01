a4b28413f15134e99aab42e1b7c6d724cea2b14e
smcdev <smcdev@outlook.com>
2023-01-01 16:52:56 -0600

    Update Undefined and Null.js

--- Functions/Undefined and Null.js
+++ Functions/Undefined and Null.js
@@ -1,8 +1,15 @@
 let name;
-name = Jen;
+name = "Jen";
 
 if (name === undefined) {
   console.log("Please provide a name");
 } else {
   console.log(name);
 }
+
+let square = function (num) {
+  console.log(num);
+};
+
+let result = square();
+console.log(result);
