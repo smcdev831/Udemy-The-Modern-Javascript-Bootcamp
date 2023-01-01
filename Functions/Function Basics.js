da6c3a8c06e085c83cbd28013c1cbfc6ff4b4b19
smcdev <smcdev@outlook.com>
2023-01-01 16:00:00 -0600

    Update Function Basics.js

---
+++ Functions/Function Basics.js
@@ -0,0 +1,18 @@
+let greetUser = function () {
+  console.log("Welcome user");
+};
+
+greetUser();
+greetUser();
+greetUser();
+
+let square = function (num) {
+  let result = num * num;
+  return result;
+};
+
+let value = square(3);
+let otherValue = square(10);
+
+console.log(value);
+console.log(otherValue);
