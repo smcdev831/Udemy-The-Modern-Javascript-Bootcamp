6a97ec97819f3faa69c746b8a19a81742eb63a52
smcdev <smcdev@outlook.com>
2023-01-01 16:06:17 -0600

    Update Function Basics.js

--- Functions/Function Basics.js
+++ Functions/Function Basics.js
@@ -25,3 +25,15 @@ let convertFahrenheitToCelsius = function (num) {
 
 convertFahrenheitToCelsius(32);
 convertFahrenheitToCelsius(68);
+
+//Teacher Solution
+convertFahrenheitToCelsius = function (fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5 / 9);
+  return celsius;
+};
+
+let tempOne = convertFahrenheitToCelsius(32);
+let tempTwo = convertFahrenheitToCelsius(68);
+
+console.log(tempOne);
+console.log(tempTwo);
