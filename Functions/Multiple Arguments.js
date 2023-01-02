87c073123aaa848bd64545b6ebc1b2f01828da8a
smcdev <smcdev@outlook.com>
2023-01-01 18:49:42 -0600

    Update Multiple Arguments.js

--- Functions/Multiple Arguments.js
+++ Functions/Multiple Arguments.js
@@ -4,3 +4,13 @@ let add = function (a, b, c) {
 
 let result = add(10, 1, 5);
 console.log(result);
+
+let getScoreText = function (name = "Anonymous", score = 0) {
+  return `Name: ${name} - Score: ${score}`;
+};
+
+// console.log(getScoreText());
+// console.log(getScoreText("Andrew", 100));
+
+let scoreText = getScoreText();
+console.log(scoreText);
