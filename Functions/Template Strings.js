c1459ad8feee36d6f1eaaff8a5d7d48ab11e54ae
smcdev <smcdev@outlook.com>
2023-01-01 19:11:57 -0600

    Update Template Strings.js

--- Functions/Template Strings.js
+++ Functions/Template Strings.js
@@ -4,3 +4,19 @@ name = "Vikram";
 let age = 12;
 
 console.log(`Hey, my name is ${name}! I am ${age} years old.`);
+
+let getScoreText = function (name = "Anonymous", score = 0) {
+  return `Name: ${name} - Score: ${score}`;
+};
+
+//Challenge
+let getTip = function (bill, tipPercent = 0.2) {
+  let percent = tipPercent * 100;
+  let tip = bill * tipPercent;
+  let total = bill * tipPercent + bill;
+  return `A ${percent}% tip on $${bill} would be $${tip} for a total of $${total}`;
+};
+
+console.log(getTip(100));
+console.log(getTip(100, 0.3));
+console.log(getTip(50, 0.1));
