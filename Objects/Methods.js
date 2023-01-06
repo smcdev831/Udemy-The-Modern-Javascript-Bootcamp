609625d85d52107382b3898ba4f8e528ec87c6c8
smcdev <smcdev@outlook.com>
2023-01-05 23:57:58 -0600

    Update Methods.js

--- Objects/Methods.js
+++ Objects/Methods.js
@@ -3,7 +3,14 @@ let restaurant = {
   guestCapacity: 75,
   guestCount: 0,
   checkAvailability: function (partySize) {
-    return restaurant.guestCapacity - restaurant.guestCount >= partySize;
+    return this.guestCapacity - this.guestCount >= partySize;
+  },
+  //Challenge Area
+  seatParty: function (partySize) {
+    this.guestCount += partySize;
+  },
+  removeParty: function (partySize) {
+    this.guestCount -= partySize;
   },
 };
 
@@ -12,3 +19,9 @@ console.log(status);
 
 status = restaurant.checkAvailability(76);
 console.log(status);
+
+restaurant.seatParty(12);
+console.log(restaurant.guestCount);
+
+restaurant.removeParty(2);
+console.log(restaurant.guestCount);
