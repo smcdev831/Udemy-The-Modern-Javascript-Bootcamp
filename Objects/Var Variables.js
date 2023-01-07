50a82379095a5921fa3d65e4d59a3aa4d8c4c77e
smcdev <smcdev@outlook.com>
2023-01-06 23:49:33 -0600

    Create Var Variables.js

---
+++ Objects/Var Variables.js
@@ -0,0 +1,11 @@
+var firstName = "Sharon";
+firstName = "John";
+console.log(firstName);
+
+var firstName = "Claudia";
+console.log(firstName);
+
+if (10 === 10) {
+  var firstName = "Tiffany";
+}
+console.log(firstName);
