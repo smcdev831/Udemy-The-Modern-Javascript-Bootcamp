bb8c755237ed43e0780cc9fb7b6e33e2950cbe53
smcdev <smcdev@outlook.com>
2023-01-06 23:16:58 -0600

    Update Number Methods.js

--- Objects/Number Methods.js
+++ Objects/Number Methods.js
@@ -23,7 +23,6 @@ let makeGuess = function (guess) {
   let min = 1;
   let max = 5;
   let randomNumber = Math.floor(Math.random() * max - min + 1) + min;
-  console.log(randomNumber);
   return guess === randomNumber;
 };
 
