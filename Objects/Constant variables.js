c41450d5a3981c3192812cc792d4a4134b946eae
smcdev <smcdev@outlook.com>
2023-01-06 23:28:52 -0600

    Update Constant variables.js

--- Objects/Constant variables.js
+++ Objects/Constant variables.js
@@ -9,3 +9,27 @@ const person = {
 
 person.age += 1;
 console.log(person);
+
+//Challenge Area
+const gradeCalculator = function (studentScore, totalPossible) {
+  const grade = (studentScore / totalPossible) * 100;
+  if (grade >= 90) {
+    letterGrade = "A";
+  } else if (grade >= 80) {
+    letterGrade = "B";
+  } else if (grade >= 70) {
+    letterGrade = "C";
+  } else if (grade >= 60) {
+    letterGrade = "D";
+  } else {
+    letterGrade = "F";
+  }
+
+  return `You got an ${letterGrade} (${grade}%)!`;
+};
+
+console.log(gradeCalculator(90, 100));
+console.log(gradeCalculator(80, 100));
+console.log(gradeCalculator(75, 100));
+console.log(gradeCalculator(63, 100));
+console.log(gradeCalculator(20, 100));
