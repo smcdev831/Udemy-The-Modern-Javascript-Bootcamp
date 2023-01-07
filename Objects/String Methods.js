3f795fbc268813e62081f6dd00c3c18165d051d9
smcdev <smcdev@outlook.com>
2023-01-06 22:56:43 -0600

    Update String Methods.js

---
+++ Objects/String Methods.js
@@ -0,0 +1,20 @@
+let name = "Sharon Moreno";
+
+console.log(name.length);
+
+//Convert to uppercase
+console.log(name.toUpperCase());
+
+//Convert to lowercase
+console.log(name.toLowerCase());
+
+//Includes
+let password = "ABC123Password098";
+console.log(name.includes("Password"));
+console.log(password.includes("Password"));
+
+//Trim
+name = "   Sharon Moreno   ";
+cleanName = name.trim();
+console.log(name.length);
+console.log(cleanName.length);
