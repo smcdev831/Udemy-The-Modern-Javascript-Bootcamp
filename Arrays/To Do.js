b5874cd442513e6534a32fa71073d65659308032
smcdev <smcdev@outlook.com>
2023-01-07 23:52:33 -0600

    Update To Do.js

--- Arrays/To Do.js
+++ Arrays/To Do.js
@@ -9,3 +9,8 @@ const todos = [
 console.log(`You have ${todos.length} todos.`);
 console.log(`Todo: ${todos[0]}`);
 console.log(`Todo: ${todos[todos.length - 2]}`);
+
+todos.splice(2, 1);
+todos.push("Call John again");
+todos.shift();
+console.log(todos);
