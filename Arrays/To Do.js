9428a3d0db65caf72e3f42dec6e2a9edeab7da93
smcdev <smcdev@outlook.com>
2023-01-07 23:53:00 -0600

    Update To Do.js

--- Arrays/To Do.js
+++ Arrays/To Do.js
@@ -6,10 +6,12 @@ const todos = [
   "Play WoW",
 ];
 
+//Challenge 1
 console.log(`You have ${todos.length} todos.`);
 console.log(`Todo: ${todos[0]}`);
 console.log(`Todo: ${todos[todos.length - 2]}`);
 
+//Challenge 2
 todos.splice(2, 1);
 todos.push("Call John again");
 todos.shift();
