5490b6bc1902cb68691ad267ba9b765cfe901311
smcdev <smcdev@outlook.com>
2023-01-07 23:35:42 -0600

    Update To Do.js

---
+++ Arrays/To Do.js
@@ -0,0 +1,11 @@
+const todos = [
+  "Call John",
+  "Practice Javascript",
+  "Exercise",
+  "Do laundry",
+  "Play WoW",
+];
+
+console.log(`You have ${todos.length} todos.`);
+console.log(`Todo: ${todos[0]}`);
+console.log(`Todo: ${todos[todos.length - 2]}`);
