eab5724d625a6f27b9a955e2ea7ced9c872b15dc
smcdev <smcdev@outlook.com>
2023-01-08 17:58:23 -0600

    Update To Do.js

--- Arrays/To Do.js
+++ Arrays/To Do.js
@@ -8,11 +8,16 @@ const todos = [
 
 //Challenge 1
 console.log(`You have ${todos.length} todos.`);
-console.log(`Todo: ${todos[0]}`);
-console.log(`Todo: ${todos[todos.length - 2]}`);
+// console.log(`Todo: ${todos[0]}`);
+// console.log(`Todo: ${todos[todos.length - 2]}`);
 
 //Challenge 2
-todos.splice(2, 1);
-todos.push("Call John again");
-todos.shift();
-console.log(todos);
+// todos.splice(2, 1);
+// todos.push("Call John again");
+// todos.shift();
+// console.log(todos);
+
+//Challenge 3
+todos.forEach(function (item, index) {
+  console.log(`Todo ${index + 1}: ${item}`);
+});
