8f35995a8113264f8b06c380ac7456988efba39a
smcdev <smcdev@outlook.com>
2023-01-10 22:37:55 -0600

    Update Expense Tracker.js

--- Arrays/Expense Tracker.js
+++ Arrays/Expense Tracker.js
@@ -1,43 +1,23 @@
 let myAccount = {
   name: "Sharon Moreno",
   expenses: [],
-  income: 0,
+  addExpense: function (description, amount) {
+    this.expenses.push({ description: description, amount: amount });
+  },
+  getAccountSummary: function () {
+    let spending = 0;
+    this.expenses.forEach(function (expense) {
+      spending += expense.amount;
+    });
+    let balance = this.income - spending;
+    console.log(`Account for ${this.name} has $${spending} in expenses.`);
+  },
 };
 
-let addExpense = function (description, amount) {
-  this.expenses.push({ description: description, amount: amount });
-};
-
-let addIncome = function (account, amount) {
-  account.income += amount;
-};
-
-let resetAccount = function (account) {
-  account.income = 0;
-  account.expenses = [];
-};
-
-let getAccountSummary = function (account) {
-  let spending = 0;
-  account.expenses.forEach((amount) => {
-    spending += amount;
-  });
-  let balance = account.income - spending;
-  console.log(
-    `Account for ${account.name} has $${balance}. $${account.income} in income. $${account.expenses} in expenses.`
-  );
-};
-
-addExpense("Monster Energy", 2.5);
+myAccount.addExpense("Monster Energy", 2.5);
 console.log(myAccount);
 
-addExpense(myAccount, "Starbucks", 5.5);
+myAccount.addExpense("Starbucks", 5.5);
 console.log(myAccount);
 
-addIncome(myAccount, 1000);
-console.log(myAccount);
-
-getAccountSummary(myAccount);
-
-resetAccount(myAccount);
-console.log(myAccount);
+myAccount.getAccountSummary();
