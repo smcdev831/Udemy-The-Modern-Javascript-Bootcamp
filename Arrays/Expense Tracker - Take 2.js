01be0b687410c49a6d63208e228c8189ea80d531
smcdev <smcdev@outlook.com>
2023-01-10 22:54:32 -0600

    Create Expense Tracker - Take 2.js

---
+++ Arrays/Expense Tracker - Take 2.js
@@ -0,0 +1,22 @@
+let myAccount = {
+  name: "Maria Moreno",
+  expenses: [],
+  addExpense: function (description, amount) {
+    this.expenses.push({ description: description, amount: amount });
+  },
+  getAccountSummary: function () {
+    let spending = 0;
+    this.expenses.forEach(function (expense) {
+      spending += expense.amount;
+    });
+    console.log(`Account for ${this.name} has $${spending} in expenses.`);
+  },
+};
+
+myAccount.addExpense("Monster Energy", 2.5);
+console.log(myAccount);
+
+myAccount.addExpense("Starbucks", 4.5);
+console.log(myAccount);
+
+myAccount.getAccountSummary();
