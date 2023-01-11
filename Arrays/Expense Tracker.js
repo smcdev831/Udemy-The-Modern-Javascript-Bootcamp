01a2935c827481f3dc2c618000719efb0da3ab9a
smcdev <smcdev@outlook.com>
2023-01-10 21:07:59 -0600

    Create Expense Tracker.js

---
+++ Arrays/Expense Tracker.js
@@ -0,0 +1,39 @@
+let myAccount = {
+  name: "Sharon Moreno",
+  expenses: 0,
+  income: 0,
+};
+
+let addExpense = function (account, amount) {
+  account.expenses += amount;
+};
+
+let addIncome = function (account, amount) {
+  account.income += amount;
+};
+
+let resetAccount = function (account) {
+  account.income = 0;
+  account.expenses = 0;
+};
+
+let getAccountSummary = function (account) {
+  let balance = account.income - account.expenses;
+  console.log(
+    `Account for ${account.name} has $${balance}. $${account.income} in income. $${account.expenses} in expenses.`
+  );
+};
+
+addExpense(myAccount, 2.5);
+console.log(myAccount);
+
+addExpense(myAccount, 2.5);
+console.log(myAccount);
+
+addIncome(myAccount, 1000);
+console.log(myAccount);
+
+getAccountSummary(myAccount);
+
+resetAccount(myAccount);
+console.log(myAccount);
