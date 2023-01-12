a1b5203629524c00df8e43c9621c445e7684cc21
smcdev <smcdev@outlook.com>
2023-01-12 00:21:00 -0600

    Update Expense Tracker.js

--- Arrays/Expense Tracker.js
+++ Arrays/Expense Tracker.js
@@ -1,15 +1,29 @@
 let myAccount = {
   name: "Sharon Moreno",
   expenses: [],
+  income: [],
   addExpense: function (description, amount) {
     this.expenses.push({ description: description, amount: amount });
   },
+  addIncome: function (description, amount) {
+    this.income.push({ description: description, amount: amount });
+  },
   getAccountSummary: function () {
     let spending = 0;
+    let incomes = 0;
     this.expenses.forEach(function (expense) {
       spending += expense.amount;
     });
-    console.log(`Account for ${this.name} has $${spending} in expenses.`);
+    this.income.forEach(function (funds) {
+      incomes += funds.amount;
+    });
+    console.log(
+      `Account for ${
+        this.name
+      } has $${spending} in expenses, and an income of $${incomes}. The balance is ${
+        incomes - spending
+      }`
+    );
   },
 };
 
@@ -17,6 +31,12 @@ myAccount.addExpense("Monster Energy", 2.5);
 console.log(myAccount);
 
 myAccount.addExpense("Starbucks", 4.5);
+console.log(myAccount);
+
+myAccount.addIncome("Payday", 1600);
+console.log(myAccount);
+
+myAccount.addIncome("Bonus", 400);
 console.log(myAccount);
 
 myAccount.getAccountSummary();
