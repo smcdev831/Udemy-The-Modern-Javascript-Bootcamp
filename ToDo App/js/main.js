5e52fada687e86ce838ebe55ab69f757f7c0efc8
smcdev <smcdev@outlook.com>
2023-01-16 21:02:50 -0600

    Update main.js

--- ToDo App/js/main.js
+++ ToDo App/js/main.js
@@ -1,1 +1,9 @@
 console.log("This is for the To Do List app");
+
+let ps = document.querySelectorAll("p");
+
+ps.forEach(function (p) {
+  if (p.textContent.includes("laundry")) {
+    p.remove();
+  }
+});
