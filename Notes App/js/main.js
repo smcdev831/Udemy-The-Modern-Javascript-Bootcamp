d8b2e6520456df0e1e0b24d8fcb3c875aa864c5c
smcdev <smcdev@outlook.com>
2023-01-16 19:37:43 -0600

    Update main.js

--- Notes App/js/main.js
+++ Notes App/js/main.js
@@ -1,1 +1,11 @@
 console.log("This is from a different file");
+
+let p = document.querySelector("p");
+// p.remove();
+
+let ps = document.querySelectorAll("p");
+ps.forEach(function (p) {
+  //   p.textContent = "**********";
+  //   console.log(p.textContent);
+  //   p.remove();
+});
