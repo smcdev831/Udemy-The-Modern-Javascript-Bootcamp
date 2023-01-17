4586c97f17668eee67ffceb283c7e3747051ec8f
smcdev <smcdev@outlook.com>
2023-01-17 00:17:41 -0600

    Update main.js

--- Notes App/js/main.js
+++ Notes App/js/main.js
@@ -4,8 +4,12 @@ let p = document.querySelector("p");
 // p.remove();
 
 let ps = document.querySelectorAll("p");
-ps.forEach(function (p) {
-  //   p.textContent = "**********";
-  //   console.log(p.textContent);
-  //   p.remove();
-});
+// ps.forEach(function (p) {
+//     p.textContent = "**********";
+//     console.log(p.textContent);
+//     p.remove();
+// });
+
+let newParagraph = document.createElement("p");
+newParagraph.textContent = "This is a new element from JS";
+document.querySelector("body").appendChild(newParagraph);
