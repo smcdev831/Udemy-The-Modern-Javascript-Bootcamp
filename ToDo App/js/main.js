619879b63ff7e290faf3f57e28d709624d72ac2d
smcdev <smcdev@outlook.com>
2023-01-17 20:02:49 -0600

    Update main.js

--- ToDo App/js/main.js
+++ ToDo App/js/main.js
@@ -1,3 +1,11 @@
+const todos = [
+  { text: "Call John", completed: false },
+  { text: "Practice Javascript", completed: false },
+  { text: "Exercise", completed: false },
+  { text: "Do laundry", completed: true },
+  { text: "Play WoW", completed: false },
+];
+
 console.log("This is for the To Do List app");
 
 let ps = document.querySelectorAll("p");
