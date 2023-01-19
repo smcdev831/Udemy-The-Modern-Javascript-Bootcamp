7789eaff2e3fd9d53ac6a6c55ad82ebd483f65fa
smcdev <smcdev@outlook.com>
2023-01-18 22:58:13 -0600

    Update main.js

--- Notes App/js/main.js
+++ Notes App/js/main.js
@@ -13,18 +13,6 @@ const notes = [
   },
 ];
 
-console.log("This is from a different file");
-
-let p = document.querySelector("p");
-// p.remove();
-
-let ps = document.querySelectorAll("p");
-// ps.forEach(function (p) {
-//     p.textContent = "**********";
-//     console.log(p.textContent);
-//     p.remove();
-// });
-
-let newParagraph = document.createElement("p");
-newParagraph.textContent = "This is a new element from JS";
-document.querySelector("body").appendChild(newParagraph);
+document.querySelector("button").addEventListener("click", function (e) {
+  e.target.textContent = "The button was clicked";
+});
