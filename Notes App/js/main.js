6042bde928cd344101d67830754d8744be031adc
smcdev <smcdev@outlook.com>
2023-01-21 17:17:51 -0600

    Update main.js

--- Notes App/js/main.js
+++ Notes App/js/main.js
@@ -41,3 +41,9 @@ document.querySelector("#search-text").addEventListener("input", function (e) {
   filters.searchText = e.target.value;
   renderNotes(notes, filters);
 });
+
+document.querySelector("#name-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(e.target.elements.firstName.value);
+  e.target.elements.firstName.value = "";
+});
