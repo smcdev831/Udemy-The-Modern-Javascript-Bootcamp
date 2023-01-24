6d1906ee320335ab12923b69aaa522b7e7e197db
smcdev <smcdev@outlook.com>
2023-01-23 21:26:29 -0600

    Update main.js

--- Notes App/js/main.js
+++ Notes App/js/main.js
@@ -42,7 +42,6 @@ document.querySelector("#search-text").addEventListener("input", function (e) {
   renderNotes(notes, filters);
 });
 
-document.querySelector("#for-fun").addEventListener("change", function (e) {
-  e.preventDefault();
-  console.log(e.target.checked);
+document.querySelector("#filter-by").addEventListener("change", function (e) {
+  console.log(e.target.value);
 });
