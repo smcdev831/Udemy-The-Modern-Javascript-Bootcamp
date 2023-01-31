abd0b5b4a053edb565d0bdf5950d598d9ffdf0e7
smcdev <smcdev@outlook.com>
2023-01-30 19:21:20 -0600

    Update functions.js

--- Notes App/js/functions.js
+++ Notes App/js/functions.js
@@ -6,3 +6,14 @@ let getSavedNotes = function () {
     return [];
   }
 };
+
+let generateNoteDOM = function (note) {
+  let noteElement = document.createElement("p");
+
+  if (note.title > 0) {
+    noteElement.textContent = note.title;
+  } else {
+    noteElement.textContent = "Untitled Note";
+  }
+  return noteElement;
+};
